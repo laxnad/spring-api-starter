5edc5f702b0a09f6cda9768ddb085287d665302e
laxman <laxnadig@gmail.com>
2026-02-16 19:23:01 +0530

    creating dynamic routes for Users api

---
+++ src/main/java/com/codewithmosh/store/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.codewithmosh.store.controller;
+
+import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
