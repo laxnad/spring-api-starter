5dc0016fa43b9cdc85c40923e5adee04dbfc1c2c
laxman <laxnadig@gmail.com>
2026-03-02 13:06:57 +0530

    creating /products api

---
+++ src/main/java/com/codewithmosh/store/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.codewithmosh.store.controller;
+
+import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.entities.Product;
+import com.codewithmosh.store.mappers.ProductMapper;
+import com.codewithmosh.store.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @GetMapping
+    public List<ProductDto> getAllProducts(){
+        List<Product> products;
+        return productRepository.findAll().stream().map(productMapper::toDto).toList();
+    }
+}
